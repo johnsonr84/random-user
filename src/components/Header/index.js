b5714df7bfb2340fbc1e88cff59e1224efc07cba
Your Name <robertcjohnson1984@gmail.com>
2021-12-24 13:21:50 -0700

    add components folder

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Header({ title }) {
+	return (
+		<header>
+			<div className="header">
+				<h1>Member Directory</h1>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
