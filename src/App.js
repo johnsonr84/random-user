b10593bd0f0b9c75ee57c006f3875cf873f78213
Your Name <robertcjohnson1984@gmail.com>
2021-12-24 14:32:24 -0700

    import api add useState and useEffect to get and set Members to table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import API from "./utils/API";
 import Header from './components/Header'
 import Table from './components/Table'
-import './App.css'
 
 function App() {
+  const [members, setMembers] = useState([]);
+  const [memberData] = useState("");
+  const [dataValue] = useState('name.first');
+  const [sortValue] = useState(-1);
+
+  useEffect(() => {
+    API.getMembers()
+      .then((res) => res.data.results)
+      .then((data) => data.map(({ name, phone, email, picture }) => ({
+        image: picture.thumbnail,
+        name,
+        phone,
+        email
+      })))
+      .then(setMembers);
+  }, []);
+
+  const memberDataRegExp = new RegExp(memberData, "i")
+  const getData = (member) => {
+    switch (dataValue) {
+      case "name.first":
+        return member.name.first
+      case "name.last":
+        return member.name.last
+      default:
+        return member[dataValue]
+    }
+  }
+
   return (
     <>
       <Header />
@@ -17,6 +46,33 @@ function App() {
             <th>Email</th>
           </tr>
         </thead>
+
+        <tbody className="table-row">
+                {members
+                  .filter((member) =>
+                    memberDataRegExp.test(member.name.first) ||
+                    memberDataRegExp.test(member.name.last) ||
+                    memberDataRegExp.test(member.email)
+                  ).sort((memberA, memberB) => {
+                    const a = getData(memberA)
+                    const b = getData(memberB)
+                    if (a < b) {
+                      return sortValue
+                    }
+                    if (a > b) {
+                      return -sortValue
+                    }
+                    return 0
+                  })
+                  .map(member => (
+                    < Table
+                      name={member.name.first + " " + member.name.last}
+                      image={member.image}
+                      phone={member.phone}
+                      email={member.email}
+                    />
+                  ))}
+              </tbody>
 
       </table>
     </>
