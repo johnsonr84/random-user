58d5b825398f991a789720760350ffa5f6c74aed
Your Name <robertcjohnson1984@gmail.com>
2021-12-26 21:57:45 -0700

    update API with const

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export default {
+const API = {
   getMembers: function() {
-    return axios.get("https://randomuser.me/api/?nat=us&results=30");
-  }
-};
+    return axios.get("https://randomuser.me/api/?nat=us&results=30")
+  },
+}
+
+export default API;
