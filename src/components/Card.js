247b012a7af083283ecac093a019f1713891f7f9
Your Name <robertcjohnson1984@gmail.com>
2021-12-26 21:49:42 -0700

    create card component

---
+++ src/components/Card.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react'
+import MemberContext from '../contexts/context'
+
+function Card() {
+  const { memberData } = useContext(MemberContext)
+  // console.log(memberData)
+
+  return (
+    <>
+      <div className="card center" style={{padding: '1.5rem 0 1.5rem 0'}}>
+        <div className="profile-img">
+          <img
+            src={memberData.image.large}
+            alt="large profile member pic"
+          ></img>
+        </div>
+        <div className="item">
+          <h2>
+            <span>{memberData.first + ' ' + memberData.last}</span>
+          </h2>
+        </div>
+
+        <div className="member-info center">
+          <div className="item">
+            <h3>Address:</h3>
+            <span>
+              {memberData.location.street.number}{' '}
+              {memberData.location.street.name}
+            </span>
+            <br></br>
+            <span>
+              {memberData.location.city}, {memberData.location.state}{' '}
+              {memberData.location.postcode}
+            </span>
+          </div>
+          <div  className="item">
+            <h3>Email:</h3>
+            <span>{memberData.email}</span>
+          </div>
+          <div className="item">
+            <h3>Date of Birth:</h3>
+            <span>{memberData.dob.date}</span>
+          </div>
+          <div className="item">
+            <h3>Phone:</h3>
+            <span>{memberData.phone}</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card
